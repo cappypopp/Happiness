f8060e6a9e622c219d95f682ab7fcafa6c828346
Cappy Popp <c.popp@yahoo.com>
2015-04-28 13:03:44 -0400

    initial checkin

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Cappy Popp on 4/22/15.
+//  Copyright (c) 2015 Cappy Popp. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
